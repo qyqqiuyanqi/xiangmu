4790ff463cba9a9331235e87ee2bb5e560628fd5
qyqqiuyanqi <1624782675@qq.com>
2019-08-31 20:36:10 +0800

    页面分析-新建登录和主页

--- src/router.js
+++ src/router.js
@@ -1,16 +1,21 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import Home from './views/Home.vue'
+import Home from './views/home/home.vue'
+import Login from './views/login/index.vue'
 
 Vue.use(Router)
 
 export default new Router({
-  routes: [
-    {
-      path: '/',
-      name: 'home',
-      component: Home
-    }
+  routes: [{
+    path: '/login',
+    component: Login
+
+  },
+  {
+    path: '/',
+    name: 'home',
+    component: Home
+  }
     // {
     //   path: '/about',
     //   name: 'about',
