5a75e974a72cfbca89d3ae0351886714f183132e
qyqqiuyanqi <1624782675@qq.com>
2019-08-31 22:00:17 +0800

    登录模块-页面布局及样式

--- src/views/login/index.vue
+++ src/views/login/index.vue
@@ -1,16 +1,50 @@
 <template>
-<div>
-    login
-</div>
-
+  <div class="login">
+    <el-card class="obox">
+      <div class="logo">
+        <img src="../../assets/img/logo_index.png" alt />
+      </div>
+      <el-form style="margin-top:15px">
+        <el-form-item>
+          <el-input placeholder="请输入手机号"></el-input>
+        </el-form-item>
+        <el-form-item>
+          <el-input placeholder="请输入验证码" style="width:230px"></el-input>
+          <!-- <el-button type="primary"  style="margin-left:25px"></el-button> -->
+          <el-button style="float:right">发送验证码</el-button>
+        </el-form-item>
+        <el-form-item>
+          <el-checkbox>我已阅读并同意用户协议和隐私条款</el-checkbox>
+        </el-form-item>
+        <el-form-item>
+          <el-button type="primary" style="width:100%">登录</el-button>
+        </el-form-item>
+      </el-form>
+    </el-card>
+  </div>
 </template>
 
 <script>
-export default {
-
-}
+export default {}
 </script>
 
-<style>
-
+<style lang="less" scoped>
+.login {
+  height: 100vh;
+  background: url("../../assets/img/login_bg.jpg");
+  background-size: cover;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  .obox {
+    width: 410px;
+    height: 320px;
+    .logo {
+      text-align: center;
+      img {
+        height: 32px;
+      }
+    }
+  }
+}
 </style>
